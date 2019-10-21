cda63e62ca1b8342014a57c8afe6badd1d986211
Holden Lally <holdenlally@gmail.com>
2019-10-20 21:10:28 -0400

    Reverted training-day-test.clj to comply with training-day tests

--- test/training_day_test.clj
+++ test/training_day_test.clj
@@ -1,10 +1,3 @@
 (ns training-day-test
-  (:require [clojure.test :refer :all]
-            [training-day :refer :all]))
-
-(deftest square-test
-  (is (= 9 (square 3))))
-
-(deftest average-test
-  (is (= 3 (average 2 4))))
+  (:use iloveponies.tests.training-day))
 
